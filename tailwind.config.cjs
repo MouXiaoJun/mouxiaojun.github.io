6819f54fa1f10273985492579163b922b81c40cd
Mia <56466008+MouXiaoJun@users.noreply.github.com>
2026-08-29 10:03:07 +0800

    build: configure Tailwind for portfolio

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./portfolio/index.html', './portfolio/src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        kanit: ['Kanit', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
